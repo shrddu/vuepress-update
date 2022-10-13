bd046f196fff7795eee821b680193618345b40d1
songhaoran <1841892879@qq.com>
2022-10-13 19:05:04 +0800

    update4

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -4,15 +4,30 @@ module.exports = {
       ['link', { rel: 'icon', href: '/avatar.png' }], // 增加一个自定义的 favicon(网页标签的图标)
     ],
     themeConfig: {
+      type:'blog',
       logo: '/avatar.png',  // 左上角logo
-      nav:[ // 导航栏配置
-        {text: 'CSDN', link: 'https://blog.csdn.net/m0_63323097?spm=1000.2115.3001.5343' },
-        {text: '投资见解', link: 'https://www.yuque.com/nieyulin/ahod2h' },
-        {text: '投资文章', link: 'https://www.yuque.com/invest/gi5ot6'},
-        {text: '前端面试题', link: 'https://www.yuque.com/nieyulin/lunpyu'},
-        {text: 'Github', link: 'https://github.com/shrddu'}
-      ],
+      nav: [
+        { text: 'Home', link: '/' },
+        { text: 'Guide', link: '/guide/' },
+        { text: 'External', link: 'https://google.com' },
+      ],  
       sidebar: 'auto', // 侧边栏配置
+      subSidebar: 'auto'//在所有页面中启用自动生成子侧边栏，原 sidebar 仍然兼容
     },
-    theme:'vuepress-theme-reco'
+    theme:'vuepress-theme-reco',
+    friendLink: [
+      {
+        title: 'vuepress-theme-reco',
+        desc: 'A simple and beautiful vuepress Blog & Doc theme.',
+        logo: "https://vuepress-theme-reco.recoluan.com/icon_vuepress_reco.png",
+        link: 'https://vuepress-theme-reco.recoluan.com'
+      },
+      {
+        title: '午后南杂',
+        desc: 'Enjoy when you can, and endure when you must.',
+        email: 'recoluan@qq.com',
+        link: 'https://www.recoluan.com'
+      },
+      // ...
+    ]
   };
